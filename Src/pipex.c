52cc2b4061e37f053667517f4f90d999c0c7d97b
andcarva <andcarva@student.42.fr>
2025-03-10 17:51:41 +0000

    the same

--- Src/pipex.c
+++ Src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: andcarva <andcarva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 16:51:57 by andcarva          #+#    #+#             */
-/*   Updated: 2025/03/10 17:34:43 by andcarva         ###   ########.fr       */
+/*   Updated: 2025/03/10 17:47:55 by andcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int	main(int ac, char **av)
 	(void)ac;
 	(void)av;
 	id = fork();
+	// printf("id: %d\n", id);
 	if (id == 0)
 	{
 		n = 1;
@@ -37,13 +38,6 @@ int	main(int ac, char **av)
 		printf("%d ", i);
 		i++;
 	}
-	// printf("id:%d\n", id);
-	// if (id != 0)
-	// {
-	// 	fork();
-	// 	printf("Hello from Main!\n");
-	// }
-	// else
-	// 	printf("Hello from Child\n");
+	printf("\n");
 	return(0);
 }
