28b13caac2a0a86a32702897f4c5bfb2b7270b45
andcarva <andcarva@student.42.fr>
2025-03-12 18:48:14 +0000

    starting child process still

--- Src/pipex.c
+++ Src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: andcarva <andcarva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 16:51:57 by andcarva          #+#    #+#             */
-/*   Updated: 2025/03/12 17:01:40 by andcarva         ###   ########.fr       */
+/*   Updated: 2025/03/12 18:36:58 by andcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ int	main(int ac, char **av, char **env)
 {
 	t_pipex	pipex;
 
+	(void)ac;
+	(void)env;
+	(void)av;
 	if (pipe(pipex.fd) == -1)
 		ft_error_file(pipex.fd);
 	pipex.pid = fork();
@@ -23,7 +26,8 @@ int	main(int ac, char **av, char **env)
 		perror("Error");
 	if (pipex.pid == 0)
 		child_process(pipex.fd);
-	else
-		parent_process(pipex.fd);
+	// else
+	// 	parent_process(pipex.fd);
+	wait(&pipex.pid);
 	return(0);
 }
--- Src/process.c
+++ Src/process.c
@@ -6,7 +6,7 @@
 /*   By: andcarva <andcarva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 16:00:03 by andcarva          #+#    #+#             */
-/*   Updated: 2025/03/12 18:06:52 by andcarva         ###   ########.fr       */
+/*   Updated: 2025/03/12 18:47:15 by andcarva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,20 @@
 
 void	child_process(int *fd)
 {
-	dup2(fd[1], STDOUT_FILENO);
+	int file_d = open("Includes/output.txt", O_RDWR);
+	
+	(void)fd;
+	printf("The original fd: %d\n", file_d);
+	if (file_d < 0)
+		printf("FUNCK\n");
+	dup2(file_d, STDOUT_FILENO);
+	ft_printf("The duplicate fd: %d\n", file_d);
+	ft_printf("Done\n");
+	close(file_d);
 	
 }
 
-void	parent_process(int *fd)
-{
+// void	parent_process(int *fd)
+// {
 	
-}
+// }
